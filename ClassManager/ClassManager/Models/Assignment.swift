9643ce0d5c2c678943b61a9f12ed6ec8e8766827
Rachel Peterson <rachelpeterson@Rachels-MacBook-Air.local>
2022-11-25 21:49:59 -0500

    add storyboard

--- ClassManager/ClassManager/Models/Assignment.swift
+++ ClassManager/ClassManager/Models/Assignment.swift
@@ -6,8 +6,6 @@ struct Assignment: Identifiable {
     var dueMonth: Int
     var dueDay: Int
     var priority: Int
-        // 0 priority = homework,
-        // 1 priority = exam
     var theme: Theme
     var className: String
     var monthDay: Int
@@ -28,15 +26,17 @@ struct Assignment: Identifiable {
 extension Assignment {
     static var sampleData: [Assignment] =
     [
-        Assignment(name: "Project 1", dueYear: 2022, dueMonth: 09, dueDay: 16, priority: 0, theme: .poppy, className: "Math"),
-        Assignment(name: "Project 2", dueYear: 2022, dueMonth: 10, dueDay: 11, priority: 0, theme: .bubblegum, className: "Science"),
-        Assignment(name: "Homework 2", dueYear: 2022, dueMonth: 10, dueDay: 13, priority: 0, theme: .bubblegum, className: "Science"),
-        Assignment(name: "English Literature Paper", dueYear: 2022, dueMonth: 10, dueDay: 18, priority: 0, theme: .yellow, className: "English"),
-        Assignment(name: "Homework 3", dueYear: 2022, dueMonth: 10, dueDay: 20, priority: 0, theme: .poppy, className: "Science"),
-        Assignment(name: "Midterm", dueYear: 2022, dueMonth: 10, dueDay: 28, priority: 1, theme: .yellow, className: "English"),
-        Assignment(name: "Project 3", dueYear: 2022, dueMonth: 11, dueDay: 14, priority: 0, theme: .sky, className: "Science"),
-        Assignment(name: "Math Final", dueYear: 2022, dueMonth: 12, dueDay: 08, priority: 1, theme: .bubblegum, className: "Math"),
-        Assignment(name: "Science Final", dueYear: 2022, dueMonth: 12, dueDay: 10, priority: 1, theme: .orange, className: "Science")
+        Assignment(name: "Project", dueYear: 2022, dueMonth: 09, dueDay: 16, priority: 0, theme: .seafoam, className: "Math"),
+        Assignment(name: "Midterm Paper", dueYear: 2022, dueMonth: 11, dueDay: 30, priority: 0, theme: .buttercup, className: "History"),
+        Assignment(name: "Project 2", dueYear: 2022, dueMonth: 10, dueDay: 11, priority: 0, theme: .lavender, className: "Science"),
+        Assignment(name: "Homework 2", dueYear: 2022, dueMonth: 10, dueDay: 13, priority: 0, theme: .lavender, className: "Science"),
+        Assignment(name: "English Literature Paper", dueYear: 2022, dueMonth: 10, dueDay: 18, priority: 0, theme: .bubblegum, className: "English"),
+        Assignment(name: "Homework 3", dueYear: 2022, dueMonth: 10, dueDay: 20, priority: 0, theme: .lavender, className: "Science"),
+        Assignment(name: "Quiz 4", dueYear: 2022, dueMonth: 10, dueDay: 14, priority: 0, theme: .buttercup, className: "History"),
+        Assignment(name: "Midterm", dueYear: 2022, dueMonth: 10, dueDay: 28, priority: 1, theme: .bubblegum, className: "English"),
+        Assignment(name: "Project 3", dueYear: 2022, dueMonth: 11, dueDay: 14, priority: 0, theme: .lavender, className: "Science"),
+        Assignment(name: "Math Final", dueYear: 2022, dueMonth: 12, dueDay: 08, priority: 1, theme: .seafoam, className: "Math"),
+        Assignment(name: "Science Final", dueYear: 2022, dueMonth: 12, dueDay: 10, priority: 1, theme: .lavender, className: "Science")
     ]
     
     static func sortAssignments() -> [Assignment] {
